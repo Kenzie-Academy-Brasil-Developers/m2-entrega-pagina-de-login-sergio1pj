19555c1f26b3a93419368bad4874fee0dbb24624
Sergio Pereira <72481029+sergio1pj@users.noreply.github.com>
2022-09-06 09:09:00 -0300

    fix: class Modal

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,13 +1,13 @@
 class Modal {
     static showModal() {
-        const modalBtn = document.querySelector('.modal-btn');
+        const modalBtn = document.getElementById('modal-btn');
         const modal = document.getElementById('modal');
         modalBtn.addEventListener('click', () => {
             modal.classList.toogle('hidden');
         });
     }
     static closeModal() {
-        const closeBtn = document.querySelector('.close');
+        const closeBtn = document.getElementById('close');
         const modal = document.getElementById('modal');
         closeBtn.addEventListener('click', () => {
             modal.classList.toogle('hidden');
